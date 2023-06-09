5bc549eade91a91367fefe8233a7e5b7f822964f
ITfrontTop <Fine1337@ukr.net>
2023-06-09 21:49:08 +0300

    added material section and swiper slider

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,18 @@ $(function () {
         slidesToShow: 3,
         slidesToScroll: 1
     });
+
+    $('.material__slider-items').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true
+    });
+});
+
+
+new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 });
